8999a623def16560961ca3068e697f7488aee542
VoidTt <notfunny35@mail.ru>
2026-06-22 06:06:56 +0300

    Update server.js

--- server.js
+++ server.js
@@ -164,60 +164,13 @@ app.post('/logout', (req, res) => {
 });
 
 // ---------- MULTIPLAYER ----------
-const players = new Map();
 
-function createPlayer(id) {
-  return {
-    id,
-    username: 'Player',
-    x: 0,
-    y: 0,
-    z: 0,
-    rotation: 0,
-    hp: 100,
-    maxHp: 100,
-    color: randomColor()
-  };
-}
 
-io.on('connection', (socket) => {
-  const player = createPlayer(socket.id);
-  players.set(socket.id, player);
 
-  socket.on('setProfile', ({ username, color }) => {
-  const p = players.get(socket.id);
-  if (!p) return;
 
-  p.username = username || p.username;
-  p.color = color || p.color;
+const setupMultiplayer = require('./socket.js');
 
-  io.emit('playerUpdated', p);
-});
-
-  socket.emit('init', {
-    id: socket.id,
-    players: Array.from(players.values())
-  });
-
-  socket.broadcast.emit('playerJoined', player);
-
-  socket.on('move', (data) => {
-  const p = players.get(socket.id);
-  if (!p) return;
-
-  p.x = data.x;
-  p.y = data.y;
-  p.z = data.z;
-  p.rotation = Number.isFinite(data.rotation) ? data.rotation : p.rotation;
-
-  socket.broadcast.emit('playerMoved', p);
-});
-
-  socket.on('disconnect', () => {
-    players.delete(socket.id);
-    io.emit('playerLeft', socket.id);
-  });
-});
+setupMultiplayer(io);
 
 server.listen(3000, '0.0.0.0', () => {
   console.log('Server running on http://0.0.0.0:3000');
