eca20840d9c4d0fbf3b71c6885487feec5b3b980
VoidTt <notfunny35@mail.ru>
2026-06-25 10:27:32 +0300

    Update server.js

--- server.js
+++ server.js
@@ -52,8 +52,10 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(3000, '0.0.0.0', () => {
-  console.log('Server is running on http://0.0.0.0:3000');
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
 
